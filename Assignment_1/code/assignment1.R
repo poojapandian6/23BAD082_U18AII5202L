bc3f5a17212b7b10feaf09adc38079de73e115c1
Pooja T <poojakiyomi@gmail.com>
2026-01-19 10:10:37 +0530

    Update assignment1.R

--- Assignment_1/code/assignment1.R
+++ Assignment_1/code/assignment1.R
@@ -1,1 +1,36 @@
+print("Student Roll No: 23BAD082")
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+df <- read.csv("C:/Users/pooja/Downloads/1.student_performance.csv")
+
+df$Subject <- as.factor(df$Subject)
+df$Final_Grade <- as.factor(df$Final_Grade)
+
+df <- df %>%
+  mutate(Average_Internal = (Internal_Test1 + Internal_Test2 + Assignment_Marks)/3)
+
+# Bar Chart
+ggplot(df %>% group_by(Subject) %>% summarise(Avg = mean(Average_Internal, na.rm = TRUE)),
+       aes(Subject, Avg, fill = Subject)) +
+  geom_bar(stat = "identity") +
+  ggtitle("Subject-wise Average Marks")
+
+# Line Chart
+ggplot(df %>%
+         summarise(Test1 = mean(Internal_Test1, na.rm = TRUE),
+                   Test2 = mean(Internal_Test2, na.rm = TRUE)) %>%
+         pivot_longer(everything(), names_to = "Test", values_to = "Marks"),
+       aes(Test, Marks)) +
+  geom_line(group = 1) +
+  geom_point() +
+  ggtitle("Performance Trend Across Tests")
+
+# Pie Chart
+ggplot(df %>% count(Final_Grade),
+       aes("", n, fill = Final_Grade)) +
+  geom_bar(stat = "identity") +
+  coord_polar("y") +
+  ggtitle("Grade Distribution") +
+  theme_void()
 
